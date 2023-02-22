067fe02205eacad3f8727b33b3634e8cdeda5a43
Kooot3 <124166824+Kooot3@users.noreply.github.com>
2023-02-22 15:49:51 +0300

    Init project

---
+++ Assets/Scripts/HangmanGame.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
+
+namespace Hangman.Scripts
+{
+    public class HangmanGame : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _textField;
+        [SerializeField] private int hp = 7;
+        [SerializeField] private TextMeshProUGUI _textLetters;
+        [SerializeField] private TextMeshProUGUI _textHp;
+        
+        private List<char> guessedLetters = new List<char>();
+        private List<char> wrongTriedLetter = new List<char>();
+        private string initialWord = "";
+        public RestartGame gameManegerWin;
+        public RestartGame gameManagerLost;
+
+        private string[] words =
+        {
+            "Apple",
+            "Dad",
+            "Deer",
+            "Tree",
+            "Pain"
+        };
+
+        private string wordToGuess = "";
+        
+        private KeyCode lastKeyPressed;
+
+        private void Start()
+        {
+            var randomIndex = Random.Range(0, words.Length);
+
+            wordToGuess = words[randomIndex];
+            
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                initialWord+= " _";
+            }
+            _textField.text = initialWord;
+            _textHp.text = "Hp left = " + hp.ToString();
+        }
+        void OnGUI()
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                // Debug.Log("Detected key code: " + e.keyCode);
+
+                if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode)
+                {
+                    ProcessKey(e.keyCode);
+                    
+                    lastKeyPressed = e.keyCode;
+                }
+            }
+        }
+
+        private void ProcessKey(KeyCode key)
+        {
+            print("Key Pressed: " + key);
+
+            char pressedKeyString = key.ToString()[0];
+
+            string wordUppercase = wordToGuess.ToUpper();
+            
+            bool wordContainsPressedKey = wordUppercase.Contains(pressedKeyString);
+            bool letterWasGuessed = guessedLetters.Contains(pressedKeyString);
+
+            if (!wordContainsPressedKey && !wrongTriedLetter.Contains(pressedKeyString))
+            {
+                wrongTriedLetter.Add(pressedKeyString);
+                hp -= 1;
+
+                if (hp <= 0)
+                {
+                    print("You Lost");
+                }
+                else
+                {
+                    _textHp.text = "Hp left = " + hp.ToString();
+                }
+            }
+            
+            if (wordContainsPressedKey && !letterWasGuessed)
+            {
+                guessedLetters.Add(pressedKeyString);
+            }
+
+            string stringToPrint = "";
+            for (int i = 0; i < wordUppercase.Length; i++)
+            {
+                char letterInWord = wordUppercase[i];
+
+                if (guessedLetters.Contains(letterInWord))
+                {
+                    stringToPrint += letterInWord;
+                }
+                else
+                {
+                    stringToPrint += "_";
+                }
+            }
+
+            if (wordUppercase == stringToPrint)
+            {
+                print("You win!");
+            }
+            
+            // print(string.Join(", ", guessedLetters));
+            print(stringToPrint);
+            _textField.text = stringToPrint;
+        }
+    }
+}
---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.SceneManagement;
+
+ namespace Hangman.Scripts
+{
+    public class RestartGame : MonoBehaviour
+
+    public GameObject gameOverUILose;
+    public GameObject gameOverUIWin;
+    
+    public void gameOverLose()
+    {
+        object gameOverUILose;
+        gameOverUILose.SetActive(true);
+    }
+
+    public void gameOverWin()
+    {
+        gameOverUIWin.SetActive(true);
+    }
+    
+    public void ResetTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
